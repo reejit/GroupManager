7820a8fe7fdbc0b24f274c4cb8573d98e60562ca
reejit <70316178+reejit@users.noreply.github.com>
2020-09-11 10:35:09 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,24 +22,15 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
 
-You know how hard it is sometimes to manage group so here is the solution for you
+I am a modular group manager bot.
 
-I'm group manager bot and Anti-spam for RR Players🃏
+Made with python by [The Checked Plugins](t.me/checkedplugins)
 
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
+Ask my commands to [them](t.me/askplugins)
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Special thanks to [this channel](t.me/checkedplugins) for helping me.
 
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
-
-Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/CHECKEDPLUGINSMANAGERBOT?startgroup=true).
 """
 
 
@@ -143,7 +134,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             send_start(bot, update)
     else:
-        update.effective_message.reply_text("I'm alive")
+        update.effective_message.reply_text("I'm ❤️zinda❤️")
 
 def send_start(bot, update):
     #Try to remove old message
@@ -157,9 +148,9 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START
 
-    keyboard = [[InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")]]
-    keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M"), 
-        InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
+    keyboard = [[InlineKeyboardButton(text="⛔ Language⛔", callback_data="set_lang_")]]
+    keyboard += [[InlineKeyboardButton(text="👁Reporting👁", callback_data="cntrl_panel_M"), 
+        InlineKeyboardButton(text="🧠Help🧠", callback_data="help_back")]]
 
     update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
